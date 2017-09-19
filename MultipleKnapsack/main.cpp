2368f257d8ad78a615df620fef86e774eaf5ee64
Rudson Mendes <rudsonm@edu.univali.br>
2017-09-19 17:19:59 -0300

    Add files via upload

--- MultipleKnapsack/main.cpp
+++ MultipleKnapsack/main.cpp
@@ -1,49 +1,90 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 #include <functional>
 #include <cstdlib>
-
-struct Item {
-	float peso;
-	float valor;
-	Item(float peso, float valor) : peso (peso), valor (valor) {}
-};
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include "utils.h"
 
 struct Mochila {
 	float capacidadeMaxima;
 	std::vector<Item> itens;
 };
 
-template <class T>
-std::vector<T> fatiarString(const std::string& s, std::function<T(const std::string&)> conversor) {
-	std::vector<T> resultado;
+using Solucao = std::vector<int>;
 
-	int inicio = 0;
-	do {
-		int k = s.find(",", inicio);
-		if (k != std::string::npos) {
-			resultado.push_back(conversor(s.substr(inicio, k)));
-			inicio = k + 1;
-		} else {
-			resultado.push_back(conversor(s.substr(inicio)));
-			inicio = -1;
+bool tentarAdicionar(const Instancia& inst, const Solucao& solucao, std::vector<int> itensDisponiveis, std::vector<float> pesosAtuais, int& itemAdicionado, int& mochilaDestino) {
+	for (int item : itensDisponiveis) {
+		for (int mochila = 0; mochila < inst.capacidades.size(); mochila++) {
+			// Se tornar inadmissivel, rejeitar
+			if (pesosAtuais[mochila] + inst.itens[item].peso > inst.capacidades[mochila]) {
+				continue;
+			}
+
+			float bonificacoesAtuais = 0;
+			for (int i = 0; i < solucao.size(); i++) {
+				if (solucao[i] >= 0) {
+					bonificacoesAtuais += inst.bonificacoes[item][i];
+				}
+			}
+
+			// As penalidades superam o valor do item
+			if (bonificacoesAtuais + inst.itens[item].valor <= 0) {
+				continue;
+			}
+
+			itemAdicionado = item;
+			mochilaDestino = mochila;
+			return true;
 		}
-	} while (inicio != -1);
-
-	return resultado;
-}
-
-int main(int argc, char** argv) {
-	std::vector<Mochila> mochilas;
-	std::vector<Item> itens;
-
-	int matriz[mochilas.size()][itens.size()];
-
-	std::vector<float> fatias = fatiarString<float>("10,2,3", [&](const std::string& s) { return std::atof(s.c_str()); });
-	for (float f : fatias) {
-		std::cout << f << "\n";
 	}
 
-	std::cout << "Hello, world\n";
+	return false;
+}
+
+void aplicarBusca(const Instancia& inst, const Solucao& solucao) {
+	float qualidade = 0;
+	bool admissivel = true;
+	std::vector<float> pesosAtuais(inst.capacidades.size(), 0);
+	std::unordered_set<int> itensDisponiveis;
+
+	for (int i = 0; i < solucao.size(); i++) {
+		int mochila = solucao[i];
+
+		if (mochila >= 0) {
+			// Incremento na qualidade e nas capacidades
+			qualidade += inst.itens[i].valor;
+			pesosAtuais[mochila] += inst.itens[i].peso;
+
+			// Verificação de admissibilidade
+			if (pesosAtuais[mochila] > inst.capacidades[mochila] && admissivel) {
+				admissivel = false;
+			}
+
+			// Cálculo das bonificações
+			for (int j = i + 1; j < solucao.size(); j++) {
+				if (solucao[j] >= 0) {
+					qualidade += inst.bonificacoes[i][j];
+				}
+			}
+		} else {
+			itensDisponiveis.insert(i);
+		}
+	}
+
+	while (true) {
+		if (admissivel) {
+
+		}
+	}	
+}
+
+
+int main(int argc, char** argv) {
+	Instancia inst("in.txt");
+	imprimirSequencia(inst.capacidades);
+
 	return 0;
 }
---
+++ MultipleKnapsack/utils.h
@@ -0,0 +1,94 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <functional>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <iostream>
+
+
+template <class Container>
+void imprimirSequencia(const Container& sequencia, std::ostream& out = std::cout) {
+	out << "[";
+	for (auto it = std::begin(sequencia); it != std::end(sequencia); ++it) {
+		if (it != std::begin(sequencia)) {
+			out << ", ";
+		}
+		out << (*it);
+	}
+	out << "]\n";
+}
+
+float toFloat(const std::string& s) {
+	return std::atof(s.c_str());
+}
+
+template <class T>
+std::vector<T> fatiarString(const std::string& s, std::function<T(const std::string&)> conversor) {
+	std::vector<T> resultado;
+
+	int inicio = 0;
+	do {
+		int k = s.find(",", inicio);
+		if (k != std::string::npos) {
+			resultado.push_back(conversor(s.substr(inicio, k)));
+			inicio = k + 1;
+		} else {
+			resultado.push_back(conversor(s.substr(inicio)));
+			inicio = -1;
+		}
+	} while (inicio != -1);
+
+	return resultado;
+}
+
+std::vector<float> obterValoresArquivo(std::ifstream &entrada) {
+	std::string linha;
+	std::getline(entrada, linha);
+	return fatiarString<float>(linha, toFloat);
+}
+
+struct Item {
+	float peso;
+	float valor;
+	Item(float peso, float valor) : peso (peso), valor (valor) {}
+};
+
+using FloatMatrix = std::vector<std::vector<float>>;
+
+struct Instancia {
+	std::vector<Item> itens;
+	std::vector<float> capacidades;
+	FloatMatrix bonificacoes;
+
+	Instancia(const std::string& arquivo) {
+		std::ifstream entrada(arquivo);
+		obterValoresArquivo(entrada); // ignorar primeira linha
+		std::vector<float> valores = obterValoresArquivo(entrada);
+		std::vector<float> pesos = obterValoresArquivo(entrada);
+
+		capacidades = obterValoresArquivo(entrada);
+
+		for (int i = 0; i < pesos.size(); i++) {
+			itens.push_back({ pesos[i], valores[i] });
+		}
+
+		bonificacoes = FloatMatrix(valores.size(), std::vector<float>(valores.size(), 0));
+
+		std::string linha;
+		while (std::getline(entrada, linha)) {
+			std::vector<float> bonus = fatiarString<float>(linha, toFloat);
+			float a = bonus[0] - 1;
+			float b = bonus[1] - 1;
+			bonificacoes[a][b] = bonus[2];
+			bonificacoes[b][a] = bonus[2];
+		}
+	}
+};
+
+#endif
